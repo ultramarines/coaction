27916f7f19ca23bff44744afff6e7602091e8b05
Ben Roaman <benroaman@gmail.com>
2015-03-06 23:06:16 -0500

    working in master like an asshole, but i got status changes and filters by status working. SHIP IT

--- migrations/versions/34e9e22f565_.py
+++ migrations/versions/34e9e22f565_.py
@@ -1,13 +1,13 @@
 """empty message
 
-Revision ID: 4ad71f40562
+Revision ID: 34e9e22f565
 Revises: None
-Create Date: 2015-03-06 12:22:59.788156
+Create Date: 2015-03-06 16:26:37.719500
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '4ad71f40562'
+revision = '34e9e22f565'
 down_revision = None
 
 from alembic import op
--- src/js/lists/task.js
+++ src/js/lists/task.js
@@ -2,9 +2,8 @@ app.factory('Task', function() {
   return function(spec) {
     spec = spec || {};
     return {
-      title: spec.title,
-      status: false,
-      assignee: spec.assignee || '',
+      title: spec.title || '',
+      status: 'new',
     };
   };
 });
